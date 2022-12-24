c2c24ebd81314549e55ee4e8d5f6260016cf7b2e
Botirjon Shokirov <botirjon@radcode.co>
2022-12-24 02:25:17 +0100

    added some

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -64,7 +64,13 @@ const Experience = () => {
                 <small className="text-light">Basic</small>
               </div>
             </article>
-
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>FireBase</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
           </div>
         </div>
       </div>
