f516ce22d0297b6a77092c6a415b9c08d1983281
Botirjon Shokirov <botirjon@radcode.co>
2022-12-26 18:17:21 +0100

    translation finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Flag from "react-world-flags";
 import {
   Header,
   Nav,
@@ -16,16 +16,42 @@ import { I18Provider, LOCALES } from "./i18n";
 
 const App = () => {
   const [locale, setLocale] = useState(LOCALES.ENGLISH);
+  const [activeNav, setActiveNav] = useState("en");
   return (
     <>
       <I18Provider locale={locale}>
-        {/* <FormattedMessage id="hello" /> */}
+        <div className="lang-btns">
+          <p
+            onClick={() => {
+              setActiveNav("en");
+              setLocale(LOCALES.ENGLISH);
+            }}
+            className={activeNav === "en" ? "active" : ""}
+          >
+            <Flag code="usa" height="15" />
+          </p>
+
+          <p
+            className={activeNav === "ru" ? "active" : ""}
+            onClick={() => {
+              setLocale(LOCALES.RUSSIAN);
+              setActiveNav("ru");
+            }}
+          >
+            <Flag code="rus" height="15" />
+          </p>
+
+          <p
+            className={activeNav === "uz" ? "active" : ""}
+            onClick={() => {
+              setLocale(LOCALES.UZBEK);
+              setActiveNav("uz");
+            }}
+          >
+            <Flag code="uzb" height="15" />
+          </p>
+        </div>
         <Header />
-        <button onClick={() => setLocale(LOCALES.ENGLISH)}>EN</button>
-        <br />
-        <button onClick={() => setLocale(LOCALES.RUSSIAN)}>RU</button>
-        <br />
-        <button onClick={() => setLocale(LOCALES.UZBEK)}>UZ</button>
         <Nav />
         <About />
         <Experience />
